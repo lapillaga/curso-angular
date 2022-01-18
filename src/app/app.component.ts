592f2657bbf523c2dbed9a275e5ac6d5ecbccef3
Luis Pillaga <lpillaga@pichincha.com>
2022-01-18 18:06:51 -0500

    finalizado clase b

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'curso-angular';
+  nombreEstudiante: string = 'Luis Pillaga';
+  edadEstudiante: number = 23;
+  horaActual: Date = new Date();
+  logAcumulador: string[] = [];
+
+  colors: Color[] = [
+    {
+      redSocial: 'Red',
+      color: '#f00'
+    },
+    {
+      redSocial: 'Green',
+      color: '#0f0'
+    },
+    {
+      redSocial: 'Blue',
+      color: '#00f'
+    }
+  ];
+
+  mostrarHora(event: any): void {
+    this.horaActual = new Date();
+    const { target } = event;
+    target.innerHTML = `Cambio la hora ${this.horaActual.getSeconds()}`;
+  }
+
+  insertLogAcumulador(log: string): void {
+    this.logAcumulador.push(log);
+  }
+
+}
+
+interface Color {
+  redSocial: string;
+  color: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { AcumuladorComponent } from './acumulador/acumulador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcumuladorComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
